0c4ecf450cd11c4adfbe47fa564b844d898dc7b1
ary oz <161294317+ARYoz@users.noreply.github.com>
2025-01-26 00:45:11 +0200

    commit - Service final

---
+++ SmartAI/src/main/java/com/example/SmartAI/SmartAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartAiApplication.class, args);
+	}
+
+}

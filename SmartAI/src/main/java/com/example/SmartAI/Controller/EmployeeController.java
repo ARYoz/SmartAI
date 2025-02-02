a524da584c3c89baa8a13e232645f29f4a4e3301
ary oz <161294317+ARYoz@users.noreply.github.com>
2025-02-02 23:40:39 +0200

    commit - Controller Employee + Shift

--- SmartAI/src/main/java/com/example/SmartAI/Controller/EmployeeController.java
+++ SmartAI/src/main/java/com/example/SmartAI/Controller/EmployeeController.java
@@ -1,4 +1,52 @@
 package com.example.SmartAI.Controller;
 
+
+import com.example.SmartAI.Model.Employee;
+import com.example.SmartAI.Service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping // Add a new Employee
+    public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee)
+    {
+        Employee newEmployee = employeeService.addEmployee(employee);
+        return ResponseEntity.ok(newEmployee);
+    }
+
+    @GetMapping // Get a one employee by his ID
+    public ResponseEntity<Employee> getEmployeeById(@RequestBody Long id)
+    {
+        Employee employee = employeeService.getEmployeeByID(id);
+        return ResponseEntity.ok(employee);
+    }
+    @GetMapping // Get all the Employees
+    public ResponseEntity<List<Employee>> getAllEmployees()
+    {
+        List<Employee> employees = employeeService.getAllEmployees();
+        return ResponseEntity.ok(employees);
+    }
+
+    @PutMapping
+    public ResponseEntity<Employee> updateEmployee(@PathVariable Long id , @RequestBody Employee UpdateEmployee)
+    {
+        Employee employee = employeeService.UpdateEmployee(id,UpdateEmployee);
+        return ResponseEntity.ok(employee);
+    }
+
+    @Deprecated
+    public ResponseEntity<String> deleteEmployee(@PathVariable Long id)
+    {
+        employeeService.DeteteEmployee(id);
+        return ResponseEntity.ok("Employee with ID:" + id + "deleted successfully.");
+    }
+
 }
--- SmartAI/src/main/java/com/example/SmartAI/Controller/ShiftController.java
+++ SmartAI/src/main/java/com/example/SmartAI/Controller/ShiftController.java
@@ -1,4 +1,74 @@
 package com.example.SmartAI.Controller;
 
+
+import com.example.SmartAI.Model.Shift;
+import com.example.SmartAI.Model.ShiftType;
+import com.example.SmartAI.Service.ShiftService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("/shift")
 public class ShiftController {
+
+    @Autowired
+    ShiftService shiftService;
+
+    @PostMapping // Add a new Shift
+    public ResponseEntity<Shift> addShift(@RequestBody Shift shift)
+    {
+        List<Shift> existingShifts = shiftService.getALlShifts();
+        Shift newShift = shiftService.addShift(shift,existingShifts);
+        return ResponseEntity.ok(newShift);
+    }
+    @GetMapping // Get all the shifts
+    public ResponseEntity<List<Shift>> getAllShifts()
+    {
+        List<Shift> shifts = shiftService.getALlShifts();
+        return ResponseEntity.ok(shifts);
+    }
+
+    @GetMapping("/{id}") // Get shift by his ID
+    public ResponseEntity<Shift> getShiftById(@PathVariable Long id)
+    {
+        Shift shift = shiftService.getShiftById(id);
+        return ResponseEntity.ok(shift);
+    }
+
+    @PutMapping // Update the shift
+    public ResponseEntity<Shift> updateShift(@PathVariable Long id , @RequestBody Shift UpdateShift)
+    {
+        Shift shift = shiftService.UpdateShift(id,UpdateShift);
+        return ResponseEntity.ok(shift);
+    }
+
+    @DeleteMapping // Delete a shift
+    public ResponseEntity<String> deleteShift(@PathVariable Long id)
+    {
+        shiftService.DeleteShift(id);
+        return ResponseEntity.ok("Shift with ID:" + id + "deleted successfully");
+    }
+
+    @GetMapping("/date/{date}") // Get shifts by thier dates
+    public ResponseEntity<List<Shift>> getShiftByDate(@PathVariable String date)
+    {
+        LocalDate parsedDate = LocalDate.parse(date);
+        List<Shift> shifts = shiftService.findByDate(parsedDate);
+        return ResponseEntity.ok(shifts);
+    }
+    @GetMapping("/type/{type}") // Get Shifts by thier type (MORNING/AFTERNON/NIGHT)
+    public ResponseEntity<List<Shift>> getShiftByType(@PathVariable ShiftType shiftType)
+    {
+        List<Shift> shifts = shiftService.findByShiftType(shiftType);
+        return ResponseEntity.ok(shifts);
+    }
+
+
+
+
+
 }
